de2184c308a1bf15cd59bbf2ce26c62d2330bf2d
Cavit Batu Soylu <CECBSofficial@hotmail.com>
2026-03-21 16:17:36 +0300

    first commit

---
+++ a301/python_tutorials_1/_10_lambda.py
@@ -0,0 +1,16 @@
+##########################################################################################
+#### Lambda Expression ###################################################################
+
+# Normal Function
+# yazarken: lambda yazısı.
+def norm_topla(x,y):
+    return x + y
+# Çıktı
+print(f"{norm_topla(10,20)}")
+
+# Lamda Expression
+topla_lambda =  lambda x,y:x+y
+# Çıktı
+print(f"{topla_lambda(10,20)}")
+
+# Python Referanslar
